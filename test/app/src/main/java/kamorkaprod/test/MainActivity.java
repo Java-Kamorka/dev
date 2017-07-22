dea5d50504968f1d2babccd6e9fe66c5d02fefc4
m0d <um-id@ya.ru>
2017-07-22 17:11:02 +0300

    add import

--- test/app/src/main/java/kamorkaprod/test/MainActivity.java
+++ test/app/src/main/java/kamorkaprod/test/MainActivity.java
@@ -1,7 +1,14 @@
 package kamorkaprod.test;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
